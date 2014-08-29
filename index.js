5c20ac6aa3e00b0bb6a9d8736e17d44605533bcc
Thomas Coopman <thomas.coopman@gmail.com>
2014-08-29 14:00:12 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var path = require('path');
+
+var Imagemin = require('imagemin');
+var loaderUtils = require('loader-utils');
+
+
+module.exports = function(content) {
+  this.cacheable && this.cacheable();
+  if(!this.emitFile) throw new Error("emitFile is required from module system");
+
+  var callback = this.async();
+
+  var name = path.basename(this.resourcePath);
+
+  var query = loaderUtils.parseQuery(this.query);
+  var options = {
+    interlaced: query.interlaced || false,
+    progressive: query.progressive || false,
+    optimizationLevel: query.optimizationLevel || 3
+  }
+
+  var imagemin = new Imagemin()
+    .src(content)
+    .use(Imagemin.gifsicle({interlaced: options.interlaced}))
+		.use(Imagemin.jpegtran({progressive: options.progressive}))
+		.use(Imagemin.optipng({optimizationLevel: options.optimizationLevel}));
+
+  imagemin.optimize(function (err, data) {
+    if (err) {
+      callback(err);
+    }
+
+    this.emitFile(name, data.contents);
+    callback(null, "module.exports = __webpack_public_path__ + " + JSON.stringify(name));
+  }.bind(this));
+};
+module.exports.raw = true;

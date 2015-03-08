8f7ec1e2581ae3f98ffe85c101e96fd520dd1db9
Thomas Coopman <thomas.coopman@gmail.com>
2015-03-08 18:21:21 +0100

    revert changes (don't hash the output, use the file-loader for that)

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-var path = require('path');
-
 var Imagemin = require('imagemin');
 var loaderUtils = require('loader-utils');
 
 
 module.exports = function(content) {
   this.cacheable && this.cacheable();
-  if (!this.emitFile) throw new Error("emitFile is required from module system");
 
   var query = loaderUtils.parseQuery(this.query);
-  var url = loaderUtils.interpolateName(this, query.name || "[hash].[ext]", {
-    context: query.context || this.options.context,
-    content: content,
-    regExp: query.regExp
-  });
   var options = {
     interlaced: query.interlaced || false,
     progressive: query.progressive || false,
     optimizationLevel: query.optimizationLevel || 3,
     bypassOnDebug: query.bypassOnDebug || false
-  }
+  };
+
+  var callback = this.async(), called = false;
 
   if (this.debug === true && options.bypassOnDebug === true) {
     // Bypass processing while on wathch mode
-    this.emitFile(url, content);
-    return "module.exports = __webpack_public_path__ + " + JSON.stringify(url);
+    return callback(null, content);
   } else {
-    var callback = this.async(), called = false;
-
     var imagemin = new Imagemin()
     .src(content)
     .use(Imagemin.gifsicle({
@@ -48,11 +39,11 @@ module.exports = function(content) {
       }
       called = true;
       if (err) {
-        return callback(err);
+        callback(err);
+      } else {
+        callback(null, files[0].contents);
       }
-      this.emitFile(url, files[0].contents);
-      callback(null, "module.exports = __webpack_public_path__ + " + JSON.stringify(url));
-    }.bind(this));
+    });
   }
 };
 module.exports.raw = true;

610a0eb4d92275134f5e2508faea52ca5368b74f
Marco Biedermann <marco.biedermann@gmx.de>
2017-11-16 20:20:12 +0100

    use ES2015 syntax

--- index.js
+++ index.js
@@ -1,97 +1,87 @@
-var imagemin = require('imagemin');
-var imageminGifsicle = require('imagemin-gifsicle');
-var imageminMozjpeg = require('imagemin-mozjpeg');
-var imageminOptipng = require('imagemin-optipng');
-var imageminSvgo = require('imagemin-svgo');
-var imageminPngquant = require('imagemin-pngquant');
-var imageminWebp = require('imagemin-webp');
-var loaderUtils = require('loader-utils');
-var assign = require('object-assign');
+const imagemin = require('imagemin');
+const imageminGifsicle = require('imagemin-gifsicle');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminOptipng = require('imagemin-optipng');
+const imageminSvgo = require('imagemin-svgo');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminWebp = require('imagemin-webp');
+const loaderUtils = require('loader-utils');
 
 /**
  * Basically the getLoaderConfig() function from loader-utils v0.2.
  */
 function getLegacyLoaderConfig(loaderContext, defaultConfigKey) {
-  var options = loaderUtils.getOptions(loaderContext);
-  var configKey = options ? options.config : defaultConfigKey;
+  const options = loaderUtils.getOptions(loaderContext);
+  const configKey = options ? options.config : defaultConfigKey;
+  
   if (configKey) {
-    return assign({}, options, loaderContext.options[configKey]);
+    return Object.assign({}, options, loaderContext.options[configKey]);
   }
+
   return options;
 }
 
-module.exports = function(content) {
+module.exports = function (content) {
   this.cacheable && this.cacheable();
 
-  var config = this.version === 2 ?
+  let config = this.version === 2 ?
     loaderUtils.getOptions(this)
-    : getLegacyLoaderConfig(this, "imageWebpackLoader");
+    : getLegacyLoaderConfig(this, 'imageWebpackLoader');
 
   if (config === null) {
     // handle the cases in which loaderUtils.getOptions() returns null
     // see https://github.com/webpack/loader-utils#getoptions
-    config = {}
+    config = {};
   }
 
-  var options = {
-    bypassOnDebug: config.bypassOnDebug || false,
-    // default optimizers
-    gifsicle: config.gifsicle || {},
-    mozjpeg: config.mozjpeg || {},
-    pngquant: config.pngquant || {},
-    optipng: config.optipng || {},
-    svgo: config.svgo || {},
-    // optional optimizers
-    webp: config.webp || null
+  const defaults = {
+    bypassOnDebug: false,
+    gifsicle: {},
+    mozjpeg: {},
+    pngquant: {},
+    optipng: {},
+    svgo: {},
+    webp: null,
   };
+
+  const options = Object.assign(defaults, config);
+
   // Remove in interlaced, progressive and optimizationLevel checks in new major version
   if (config.hasOwnProperty('interlaced')) {
     options.gifsicle.interlaced = config.interlaced;
     this.emitWarning("DEPRECATED. Configure gifsicle's interlaced option in its own options. (gifsicle.interlaced)");
   }
+
   if (config.hasOwnProperty('progressive')) {
     options.mozjpeg.progressive = config.progressive;
     this.emitWarning("DEPRECATED. Configure mozjpeg's progressive option in its own options. (mozjpeg.progressive)");
   }
+
   if (config.hasOwnProperty('optimizationLevel')) {
     options.optipng.optimizationLevel = config.optimizationLevel;
     this.emitWarning("DEPRECATED. Configure optipng's optimizationLevel option in its own options. (optipng.optimizationLevel)");
   }
 
-  var callback = this.async(),
-    called = false;
+  const callback = this.async();
 
   if (this.debug === true && options.bypassOnDebug === true) {
     // Bypass processing while on watch mode
     return callback(null, content);
-  } else {
-    var plugins = [];
-    // default optimizers
-    if(options.gifsicle.enabled !== false)
-      plugins.push(imageminGifsicle(options.gifsicle));
-    if(options.mozjpeg.enabled !== false)
-      plugins.push(imageminMozjpeg(options.mozjpeg));
-    if(options.svgo.enabled !== false)
-      plugins.push(imageminSvgo(options.svgo));
-    if(options.pngquant.enabled !== false)
-      plugins.push(imageminPngquant(options.pngquant));
-    if(options.optipng.enabled !== false)
-      plugins.push(imageminOptipng(options.optipng));
-    // optional optimizers
-    if(options.webp)
-      plugins.push(imageminWebp(options.webp));
-
-    imagemin
-      .buffer(content, {
-        plugins
-      })
-      .then(data => {
-        callback(null, data);
-      })
-      .catch(err => {
-        callback(err);
-      });
   }
+  const plugins = [];
+  // default optimizers
+  if (options.gifsicle.enabled) { plugins.push(imageminGifsicle(options.gifsicle)); }
+  if (options.mozjpeg.enabled) { plugins.push(imageminMozjpeg(options.mozjpeg)); }
+  if (options.svgo.enabled) { plugins.push(imageminSvgo(options.svgo)); }
+  if (options.pngquant.enabled) { plugins.push(imageminPngquant(options.pngquant)); }
+  if (options.optipng.enabled) { plugins.push(imageminOptipng(options.optipng)); }
+  // optional optimizers
+  if (options.webp) { plugins.push(imageminWebp(options.webp)); }
+
+  imagemin
+    .buffer(content, { plugins })
+    .then(data => callback(null, data))
+    .catch(err => callback(err));
 };
 
 module.exports.raw = true;
--- test/webpack2.config.js
+++ test/webpack2.config.js
@@ -1,52 +1,50 @@
-'use strict';
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
 
-var assetsPath = path.join(__dirname, 'public/assets');
+const assetsPath = path.join(__dirname, 'public/assets');
 
-var loaderOptions = {
+const loaderOptions = {
   mozjpeg: {
-    quality: 65
+    quality: 65,
   },
-  pngquant:{
-    quality: "65-90",
-    speed: 4
+  pngquant: {
+    quality: '65-90',
+    speed: 4,
   },
-  svgo:{
+  svgo: {
     plugins: [
       {
-        removeViewBox: false
+        removeViewBox: false,
       },
       {
-        removeEmptyAttrs: false
-      }
-    ]
+        removeEmptyAttrs: false,
+      },
+    ],
   },
   gifsicle: {
     optimizationLevel: 7,
-    interlaced: false
+    interlaced: false,
   },
   optipng: {
     optimizationLevel: 7,
-    interlaced: false
+    interlaced: false,
   },
   webp: {
-    quality: 75
-  }
-}
+    quality: 75,
+  },
+};
 
-var fileLoaderOptions = {
+const fileLoaderOptions = {
   hash: 'sha512',
   digest: 'hex',
-  name: '[hash].[ext]'
-}
+  name: '[hash].[ext]',
+};
 
 module.exports = [
   {
     entry: './test/app.js',
     output: {
       path: assetsPath,
-      filename: 'app.[hash].js'
+      filename: 'app.[hash].js',
     },
     module: {
       rules: [{
@@ -54,23 +52,23 @@ module.exports = [
         use: [
           {
             loader: 'file-loader',
-            options: fileLoaderOptions
+            options: fileLoaderOptions,
           },
           {
             loader: require.resolve('../'),
-            options: loaderOptions
-          }
-        ]
+            options: loaderOptions,
+          },
+        ],
       }, {
         test: /\.bmp$/i,
         use: [
           {
             loader: 'file-loader',
-            options: fileLoaderOptions
+            options: fileLoaderOptions,
           },
-          require.resolve('../') // loaderUtils.getOptions() returns null for this one
-        ]
-      }]
-    }
-  }
+          require.resolve('../'), // loaderUtils.getOptions() returns null for this one
+        ],
+      }],
+    },
+  },
 ];
